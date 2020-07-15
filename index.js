7fdfcd5935a069ada3919d91222992f2d0ca2e3b
Eric C <erix2205@gmail.com>
2020-07-15 09:34:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 function theBeatlesPlay(musicians, instruments){
   array = []
   for (i = 0 ; i < musicians.length ; i++) {
-    array.push(musicians[i] + "plays" + instruments[i]);
+    var l = musicians[i] + "plays" + instruments[i];
+    array.push(l)
   }
   return array
 }
