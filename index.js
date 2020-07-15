563c075aa384c66deffb47cb1910427876dee948
Eric C <erix2205@gmail.com>
2020-07-15 09:33:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  array = []
+  for (i = 0 ; i < musicians.length ; i++) {
+    array.push(musicians[i] + "plays" + instruments[i]);
+  }
+  return array
+}
