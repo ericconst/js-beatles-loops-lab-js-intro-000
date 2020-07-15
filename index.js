2c6161a4e4b758e5594db68ab5c0bd8d2c569a4b
Eric C <erix2205@gmail.com>
2020-07-15 09:51:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,12 @@ function johnLennonFacts(array){
   }
   return newArray
 }
+
+function iLoveTheBeatles(number){
+  var array = []
+  do {
+    array.push("I love the Beatles!");
+    number++;
+  } while number < 15;
+  return array
+}
